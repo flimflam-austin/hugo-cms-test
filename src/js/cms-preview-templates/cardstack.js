cff373a0653623dc83c64b73cbdb15b4d0d36ad7
Austin <austin@flimflamfactory.com>
2021-02-23 13:32:37 -0600

    n

--- src/js/cms-preview-templates/cardstack.js
+++ src/js/cms-preview-templates/cardstack.js
@@ -6,7 +6,7 @@ export default class CardPreview extends React.Component {
 		const getAsset = this.props.getAsset;
 		const widgetFor = this.props.widgetFor;
 
-		let colorClass = `cardstack cardstack--${entry.getIn([
+		let colorClass = `cardstack cardstack--spread cardstack--${entry.getIn([
 			"data",
 			"color_palette_classname",
 		])}`;
