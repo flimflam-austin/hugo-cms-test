d3272be47614663acc0b6254a32c56b1e43a08e1
Austin <austin@flimflamfactory.com>
2021-03-16 11:51:37 -0500

    Update index.js

--- plugins/netlify-sanity-md/index.js
+++ plugins/netlify-sanity-md/index.js
@@ -1,7 +1,7 @@
 // https://www.sanity.io/guides/sanity-and-hugo-with-netlify-plugins
 
 module.exports = {
-	onPreBuild: async ({ utils, packagejson }) => {
+	onPreBuild: async ({ utils }) => {
 		console.log("my plugin loaded!");
 
 		try {
